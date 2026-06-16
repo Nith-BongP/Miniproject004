9b9b630e3deb9d2048a29e7f20d25c88f37b13e9
narinkhmermorn <narinroeun3@gmail.com>
2026-06-16 14:37:47 +0700

    hh

--- Controller.java
+++ Controller.java
@@ -1,12 +1,23 @@
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
-public class Controller { 
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ToggleGroup Department;
 
     @FXML
     private CheckBox check1;
@@ -33,9 +44,21 @@ public class Controller {
     private RadioButton radio3;
 
     @FXML
-    private Label result;
+    private Button save;
 
     @FXML
-    private Button save;
+    void initialize() {
+        assert Department != null : "fx:id=\"Department\" was not injected: check your FXML file 'View.fxml'.";
+        assert check1 != null : "fx:id=\"check1\" was not injected: check your FXML file 'View.fxml'.";
+        assert check2 != null : "fx:id=\"check2\" was not injected: check your FXML file 'View.fxml'.";
+        assert check3 != null : "fx:id=\"check3\" was not injected: check your FXML file 'View.fxml'.";
+        assert date != null : "fx:id=\"date\" was not injected: check your FXML file 'View.fxml'.";
+        assert name != null : "fx:id=\"name\" was not injected: check your FXML file 'View.fxml'.";
+        assert radio1 != null : "fx:id=\"radio1\" was not injected: check your FXML file 'View.fxml'.";
+        assert radio2 != null : "fx:id=\"radio2\" was not injected: check your FXML file 'View.fxml'.";
+        assert radio3 != null : "fx:id=\"radio3\" was not injected: check your FXML file 'View.fxml'.";
+        assert save != null : "fx:id=\"save\" was not injected: check your FXML file 'View.fxml'.";
+
+    }
 
 }
