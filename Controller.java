16eba43babae75c080965ea28704e5e6b3e7427e
narinkhmermorn <narinroeun3@gmail.com>
2026-06-16 13:40:54 +0700

    fjdl

--- Controller.java
+++ Controller.java
@@ -1,3 +1,37 @@
-public class Controller {
-    
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+public class Controller{
+
+    @FXML
+    private CheckBox check1;
+
+    @FXML
+    private CheckBox check2;
+
+    @FXML
+    private CheckBox check3;
+
+    @FXML
+    private DatePicker date;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private RadioButton radio1;
+
+    @FXML
+    private RadioButton radio2;
+
+    @FXML
+    private RadioButton radio3;
+
+    @FXML
+    private Button save;
+
 }
